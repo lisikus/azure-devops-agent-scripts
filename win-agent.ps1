e9c01f6a5b8ce95081615f0ac83ca49ce9c8fc03
lisikus <lisa.postarnak@gmail.com>
2019-10-18 10:00:32 +0300

    Update win-agent.ps1

--- win-agent.ps1
+++ win-agent.ps1
@@ -1,9 +1,9 @@
 #Download agent's files
-wget https://vstsagentpackage.azureedge.net/agent/2.158.0/vsts-agent-win-x64-2.158.0.zip -UseBasicParsing -outfile $HOME\Downloads\vsts-agent-win-x64-2.158.0.zip
+wget https://vstsagentpackage.azureedge.net/agent/2.158.0/vsts-agent-win-x64-2.158.0.zip -UseBasicParsing -outfile C:\Users\agentadmin\Downloads\vsts-agent-win-x64-2.158.0.zip
 
 #Unpack files
 mkdir C:\agent ; cd C:\agent
-Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-2.158.0.zip", "$PWD")
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("C:\Users\agentadmin\Downloads\vsts-agent-win-x64-2.158.0.zip", "$PWD")
 
 #Configure agent
 .\config.cmd --unattended --url $1 --auth pat --token $2 --pool $3 --agent $4  --work ./_work --runAsService 
